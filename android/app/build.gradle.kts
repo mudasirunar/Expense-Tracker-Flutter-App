f9b2efd07c919f2f5d579b0a3915562655563f54
Mudasir Ali <unarmudasir@gmail.com>
2026-09-15 08:15:13 +0500

    chore: update application package name and bundle identifier

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.expense_tracker"
+    namespace = "com.mudasir.expensetracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -14,9 +14,13 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.expense_tracker"
+        applicationId = "com.mudasir.expensetracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/mudasir/expensetracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/mudasir/expensetracker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.expense_tracker
+package com.mudasir.expensetracker
 
 import io.flutter.embedding.android.FlutterActivity
 
